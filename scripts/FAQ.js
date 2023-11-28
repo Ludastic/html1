b36c0a482e384481d12b71ec0d72e5cf9deeb2e8
Ludastic <105712438+Ludastic@users.noreply.github.com>
2023-11-28 17:17:13 +0300

    moved from jquerry to vanilla js

--- scripts/FAQ.js
+++ scripts/FAQ.js
@@ -1,22 +1,32 @@
-$(".question-wrapper").click( function () {
-    var container = $(this).parents(".accordion");
-    var answer = container.find(".answer-wrapper");
-    var trigger = container.find(".material-icons.drop");
-    var state = container.find(".question-wrapper");
-    
-    answer.animate({height: "toggle"}, 100);
-    
-    if (trigger.hasClass("icon-expend")) {
-      trigger.removeClass("icon-expend");
+let FAQs = document.querySelectorAll(".question-wrapper");
+FAQs.forEach(element => {
+  element.onclick = function () {
+    console.log('amogus');
+    var container = element.parentNode;
+    var answer = container.querySelector(".answer-wrapper");
+    var trigger = container.querySelector(".material-icons.drop");
+    var state = container.querySelector(".question-wrapper");
+    var answerText = container.querySelector(".answer");
+
+    if (answerText.hidden) {
+      answer.style.height = `${80}px`;
+    } else {
+      answer.style.height = `${0}px`;
+    }
+    answerText.hidden = !answerText.hidden;
+
+    if (trigger.classList.contains("icon-expend")) {
+      trigger.classList.remove("icon-expend");
     }
     else {
-      trigger.addClass("icon-expend");
+      trigger.classList.add("icon-expend");
     }
-    
-    if (container.hasClass("expanded")) {
-      container.removeClass("expanded");
+
+    if (container.classList.contains("expanded")) {
+      container.classList.remove("expanded");
     }
     else {
-      container.addClass("expanded");
+      container.classList.add("expanded");
     }
-  });
+  }
+});

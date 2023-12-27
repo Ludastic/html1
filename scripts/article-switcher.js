f6b093c14c3aafb23a22444cd1e99240fa8c87c8
Ludastic <titlyanov2004@gmail.com>
2023-12-27 14:27:20 +0300

    update 2 articles

--- scripts/article-switcher.js
+++ scripts/article-switcher.js
@@ -4,15 +4,11 @@ let articleVids = document.getElementById("videos");
 let articleHeadliners = document.getElementById("headliners");
 
 buttonForHeadline.onclick = (ev) => {
-    buttonForHeadline.style.color = "#000000";
-    buttonForVids.style.color = "rgba(31,31,31,.4)";
     articleHeadliners.style.display = "block";
     articleVids.style.display = "none";
 }
 
 buttonForVids.onclick = (ev) => {
-    buttonForHeadline.style.color = "rgba(31,31,31,.4)";
-    buttonForVids.style.color = "#000000";
     articleHeadliners.style.display = "none";
     articleVids.style.display = "block";
 }
